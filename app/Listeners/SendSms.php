daf2c2c4584759b88a8f0d423da291262862cf17
ashokkumar2342 <ashokkumar2342@gmail.com>
2019-11-25 21:28:21 +0530

    Update SendSms.php

--- app/Listeners/SendSms.php
+++ app/Listeners/SendSms.php
@@ -73,13 +73,13 @@ class SendSms
        //  Log::info($event->mobile.' : '.$event->message);
 
 
-        // $msg=urlencode($event->message); 
-        // $url = "http://smsdealnow.com/api/pushsms.php?usr=516001&key=010Av2NN20YRxgyh6z4KMf01owU8Ry&sndr=ZGSRTK&ph=$event->mobile&text=$msg&rpt=1";
-        // $ch = curl_init($url);
-        // curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        // $curl_scraped_page = curl_exec($ch);
-        // curl_close($ch); 
-        Log::info($event->mobile.' : '.$event->message);    
+        $msg=urlencode($event->message); 
+        $url = "http://smsdealnow.com/api/pushsms.php?usr=516001&key=010Av2NN20YRxgyh6z4KMf01owU8Ry&sndr=ZGSRTK&ph=$event->mobile&text=$msg&rpt=1";
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        $curl_scraped_page = curl_exec($ch);
+        curl_close($ch); 
+        Log::info($event->mobile.' : '.$msg);    
          
     }
 }
