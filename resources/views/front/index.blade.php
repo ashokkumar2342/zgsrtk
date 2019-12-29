906d0c654a54ddd2b5066325f8f0d3be9de829e4
ashokkumar2342 <ashokkumar2342@gmail.com>
2019-12-29 14:17:00 +0530

    Update index.blade.php

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -516,7 +516,7 @@
 @endif
 <script type="text/javascript">  
      $(window).load(function(){        
-     // $('#myModal').modal('show');
+      $('#myModal').modal('show');
    }); 
    
 </script>
