a21544f33117cd9ba731ece665772a9fab41c390
ashokkumar2342 <ashokkumar2342@gmail.com>
2020-01-14 20:07:33 +0530

    Update DbBackup.php

--- app/Console/Commands/DbBackup.php
+++ app/Console/Commands/DbBackup.php
@@ -39,7 +39,7 @@ class DbBackup extends Command
     public function handle()
     {
         $filename = "backup-" . Carbon::now()->format('Y-m-d') . ".gz";
-                $command = "mysqldump --user=" . env('DB_USERNAME') ." --password=" . env('DB_PASSWORD') . " --host=" . env('DB_HOST') . " " . env('DB_DATABASE') . "  | gzip > " . storage_path() . "/app/backup/" . $filename;
+                $command = "mysqldump --user=" . env('DB_USERNAME') ." --password=" . env('DB_PASSWORD') . " --host=" . env('DB_HOST') . " " . env('DB_DATABASE') . "  | gzip > " . storage_path() . "/app/" . $filename;
                 $returnVar = NULL;
                 $output  = NULL;
                 exec($command, $output, $returnVar);
