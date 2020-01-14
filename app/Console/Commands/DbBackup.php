f73b1b67ec567fda9a416abc8a0c2a4bf6a8268d
ashokkumar2342 <ashokkumar2342@gmail.com>
2020-01-14 23:23:22 +0530

    Update DbBackup.php

--- app/Console/Commands/DbBackup.php
+++ app/Console/Commands/DbBackup.php
@@ -32,11 +32,11 @@ class DbBackup extends Command
     {
         parent::__construct();
         $this->process = new Process(sprintf(
-                    'mysqldump -u%s -p%s %s > %s',
+                    'mysqldump -u%s -p%s %s gzip > %s',
                     config('database.connections.mysql.username'),
                     config('database.connections.mysql.password'),
                     config('database.connections.mysql.database'),
-                    storage_path('app/backup-' . Carbon::now()->format('Y-m-d') . '.zip')
+                    storage_path('app/backup-'.Carbon::now()->format('Y-m-d').'.gz')
                 ));
     }
 
@@ -55,6 +55,10 @@ class DbBackup extends Command
         try {
             $this->process->mustRun();
             $this->info('The backup has been proceed successfully.');
+            if ($process->isSuccessful())
+            {
+                $this->sendEmail(storage_path('app/backup-'.Carbon::now()->format('Y-m-d').'.gz'));
+            }
         } catch (ProcessFailedException $exception) {
             $this->error('The backup process has been failed.');
         }
