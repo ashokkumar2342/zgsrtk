0d9ed4a9e718aada03797edcfcdc5ac27c4b4de2
ashokkumar2342 <ashokkumar2342@gmail.com>
2020-01-14 22:39:28 +0530

    Update DbBackup.php

--- app/Console/Commands/DbBackup.php
+++ app/Console/Commands/DbBackup.php
@@ -36,7 +36,7 @@ class DbBackup extends Command
                     config('database.connections.mysql.username'),
                     config('database.connections.mysql.password'),
                     config('database.connections.mysql.database'),
-                    storage_path('app/backup-' . Carbon::now()->format('Y-m-d') . '.gz')
+                    storage_path('app/backup-' . Carbon::now()->format('Y-m-d') . '.zip')
                 ));
     }
 
@@ -55,11 +55,10 @@ class DbBackup extends Command
         try {
             $this->process->mustRun();
             $this->info('The backup has been proceed successfully.');
-            $this->sendEmail(storage_path('app/backup-' . Carbon::now()->format('Y-m-d') . '.gz'));
         } catch (ProcessFailedException $exception) {
             $this->error('The backup process has been failed.');
         }
-            
+            // $this->sendEmail(storage_path() . "/app/" . $filename);
     }
 
     public function sendEmail($path){
